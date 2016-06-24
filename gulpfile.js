fb1203f33a010200d7d4ef2e3e2c2d1170b39e17
unknown <Brandon.Bosco@CARES-LT-047.CARES-DEV.Local>
2016-06-24 16:35:00 -0500

    Added minification of JS files and FTP pushing capabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Include Gulp
 var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
 
 // Include plugins
 var plugins = require("gulp-load-plugins")({
@@ -7,22 +8,67 @@ var plugins = require("gulp-load-plugins")({
     replaceString: /\bgulp[\-.]/
 });
 
+//Relivent paths
+var path = {
+    root: 'public/',
+    appSource: 'public/app/',
+    assetSource: 'public/assets/',
+    jsDestination: 'public/assets/js/',
+    cssDestination: 'public/assets/css/',
+};
+
+var name = {
+    js_AngularApp: 'app.js',
+    js_LibraryBundle: 'lib.js',
+    js_min_LibraryBundle: 'lib.min.js'
+
+}
+var user = 'bjbosco@brandonbos.co';
+var pass = 'c1Ms01337!panel';
+var host = 'brandonbos.co';
+var port = 21;
+var remoteFolder = '/public_html';
+
 // Define default destination folder
-var dest = 'assets/js/';
+var dest = 'public/assets/js/';
 
 gulp.task('js', function () {
 
-    var jsFiles = ['bower_components/*'];
-    console.log(plugins.mainBowerFiles());
-    gulp.src(plugins.mainBowerFiles())
-	.pipe(plugins.filter('*.js'))
-	.pipe(plugins.concat('test.js'))
+    //console.log(plugins.mainBowerFiles('**/*.js'));
+
+    //outputs lib and lib-min
+    gulp.src(plugins.mainBowerFiles('**/*.js'))
+	.pipe(plugins.concat(name.js_LibraryBundle))
+    .pipe(plugins.minify())
 	.pipe(gulp.dest(dest));
 
-    //gulp.src(plugins.mainBowerFiles())
-	//	.pipe(plugins.filter('*.js'))
-	//	.pipe(plugins.concat('main.js'))
-	//	.pipe(plugins.uglify())
-	//	.pipe(gulp.dest(dest + 'js'));
+
+    //outputs app.js for all custom angular stuff
+    gulp.src(path.appSource + '**/*.js')
+	.pipe(plugins.concat(name.js_AngularApp))
+	.pipe(gulp.dest(dest));
 
 });
+
+gulp.task('ftp-deploy', function () {
+
+    var conn = getFtpConnection();
+
+    return gulp.src(path.root +'/**/', { buffer: false })
+        .pipe(conn.newer(remoteFolder)) // only upload newer files 
+        .pipe(conn.dest(remoteFolder));
+});
+
+gulp.task('default', ['js', 'css']);
+
+//TOOLS
+function getFtpConnection() {
+    return ftp.create({
+        host: host,
+        port: port,
+        user: user,
+        password: pass,
+        parallel: 5,
+        log: plugins.log
+    });
+}
---
+++ public/assets/js/app.js
@@ -0,0 +1,3 @@
+//Module
+//route
+//Navbar controller
